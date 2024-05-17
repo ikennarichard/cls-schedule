917cc49fb36b6a8dba07637649016d623c740907
Ikenna Richard <oguejioforrichard@gmail.com>
2024-05-17 08:57:45 +0100

    Add app test

---
+++ src/test/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App.jsx';
+import Providers from './test-utils.js';
+
+it('renders the app with the student component', () => {
+  render(
+    <Providers>
+      <App/>
+    </Providers>
+
+);
+  expect(screen.getByTestId('submit')).toBeInTheDocument();
+})
