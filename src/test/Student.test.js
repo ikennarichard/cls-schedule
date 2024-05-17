7609e8079070e43f677cf45078db55985d0ab959
Ikenna Richard <oguejioforrichard@gmail.com>
2024-05-17 08:54:54 +0100

    Add student component test

---
+++ src/test/Student.test.js
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import{ Students } from '../components/Students.jsx';
+import Providers from './test-utils.js';
+import userEvent from '@testing-library/user-event';
+
+beforeEach(() => {
+  render(
+    <Providers>
+      <Students/>
+    </Providers>
+);
+});
+
+it('renders correctly', () => {
+  expect(screen.getByText(/anna/i)).toBeInTheDocument();
+});
+
+it('deletes a subject', async () => {
+  await userEvent.click(screen.getByTestId('delete-mathematics'));
+  expect(screen.queryByTestId('delete-mathematics')).toBeNull();
+});
+
+it('adds a new subject to the schedule', async () => {
+
+  userEvent.selectOptions(screen.getByLabelText('Subject:'), 'Science');
+  userEvent.selectOptions(screen.getByLabelText('Mentor:'), 'Mr. Peter');
+  userEvent.selectOptions(screen.getByLabelText('Time:'), '10:30 - 11:00');
+ 
+  await userEvent.click(screen.getByTestId('submit'));
+  
+  expect(screen.getByTestId('delete-science')).toBeInTheDocument();
+  expect(screen.getByText(/^Class Mentor - Mr. Peter$/)).toBeInTheDocument();
+});
